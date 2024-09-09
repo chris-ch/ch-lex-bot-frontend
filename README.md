b59c8f6f12d64e83b880cbbfa4fa95bd8ff4b146
Christophe <chris-ch@users.noreply.github.com>
2024-09-09 19:04:00 +0000

    dev

--- README.md
+++ README.md
@@ -4,10 +4,38 @@ A simple Streamlit front-end for accessing Swiss legal documents.
 
 [![Open in Streamlit](https://static.streamlit.io/badges/streamlit_badge_black_white.svg)](https://chatbot-template.streamlit.app/)
 
-### How to run it on your own machine
+## How to run it on your own machine
 
 Run the app
 
    ```
    $ poetry run streamlit run scripts/bot.py
    ```
+
+## Development
+
+### Localization
+
+Creating `messages.pot` file
+
+```bash
+xgettext -o resources/messages.pot src/runbot.py
+```
+
+Merging 
+
+```bash
+msgmerge --update resources/locale/de/LC_MESSAGES/messages.po resources/messages.pot
+msgmerge --update resources/locale/en/LC_MESSAGES/messages.po resources/messages.pot
+msgmerge --update resources/locale/fr/LC_MESSAGES/messages.po resources/messages.pot
+msgmerge --update resources/locale/it/LC_MESSAGES/messages.po resources/messages.pot
+```
+
+Compiling translations to `.mo` from `.po`:
+
+```bash
+msgfmt resources/locale/de/LC_MESSAGES/messages.po -o resources/locale/de/LC_MESSAGES/messages.mo
+msgfmt resources/locale/en/LC_MESSAGES/messages.po -o resources/locale/en/LC_MESSAGES/messages.mo
+msgfmt resources/locale/fr/LC_MESSAGES/messages.po -o resources/locale/fr/LC_MESSAGES/messages.mo
+msgfmt resources/locale/it/LC_MESSAGES/messages.po -o resources/locale/it/LC_MESSAGES/messages.mo
+```
--- src/runbot.py
+++ src/runbot.py
@@ -1,60 +1,149 @@
 
+import os
 import streamlit
+import gettext
+
+from mistralai import Mistral, SystemMessage, UserMessage
+
 from helpers import string_to_stream
 
 
+def load_translations(lang):
+    localedir = os.path.abspath(os.path.join(os.path.curdir, 'resources', 'locale'))
+    if not os.path.exists(localedir):
+        raise BaseException(f"Locale folder not found: {localedir}")
+    translate = gettext.translation('messages', localedir, languages=[lang])
+    translate.install()
+    return translate.gettext
+
+
 def start():
+    # Initialize session state
+    if "language" not in streamlit.session_state:
+        streamlit.session_state.language = "en"
+    if "user_description" not in streamlit.session_state:
+        streamlit.session_state.user_description = ""
+    if "previous_user_description" not in streamlit.session_state:
+        streamlit.session_state.previous_user_description = ""
+    
+    streamlit.markdown("""
+        <style>
+        .language-selector {
+            position: fixed;
+            top: 10px;
+            right: 10px;
+            z-index: 1000;
+        }
+        .language-selector .stSelectbox {
+            min-width: 50px;
+            max-width: 50px;
+        }
+        .language-selector .stSelectbox > div > div {
+            padding: 2px;
+        }
+        </style>
+        """, unsafe_allow_html=True)
+    
+    language_options = {
+        "🇩🇪": "de",
+        "🇬🇧": "en",
+        "🇫🇷": "fr",
+        "🇮🇹": "it"
+    }
+    
+    col1, col2 = streamlit.columns([7, 1])  # Adjust the ratio as needed
+
+    # In the first (narrower) column, place your selectbox
+    with col1:
+         streamlit.write(" ")
+
+    # In the second (wider) column, place your other content
+    with col2:        
+        # You can add more elements here as needed
+        with streamlit.container():
+            streamlit.markdown('<div class="language-selector">', unsafe_allow_html=True)
+            selected_flag = streamlit.selectbox(
+                " ",
+                options=list(language_options.keys()),
+                format_func=lambda x: x,
+                key='language_selector',
+                label_visibility="collapsed"
+            )
+            streamlit.markdown('</div>', unsafe_allow_html=True)
+    
+    if streamlit.session_state.language != language_options[selected_flag]:
+        streamlit.session_state.language = language_options[selected_flag]
+        streamlit.rerun()
+        
+    _ = load_translations(streamlit.session_state.language)
     
     # Show title and description.
     streamlit.title("💬 Lex Bot 🇨🇭")
     streamlit.write(
-        "This is a simple chatbot that simply echoes your inputs to generate responses. "
-        "TODO: connect to backend."
+        _("Welcome to our virtual legal assistant.")
     )
-
-    # Ask user for their OpenAI API key via `streamlit.text_input`.
-    # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
-    # via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-    mistral_api_key = streamlit.text_input("Mistral API Key", type="password")
+    streamlit.write(
+        _("Please briefly describe your legal situation below.")
+    )
+    streamlit.write(
+        _("Our system will analyze your case and provide you with "
+          "relevant references to applicable legal texts.")
+    )
+    mistral_api_key = os.getenv("MISTRAL_API_KEY")
     if not mistral_api_key:
-        streamlit.info("Please add your Mistral API key to continue.", icon="🗝️")
+        mistral_api_key = streamlit.text_input("Mistral API Key", type="password")
+    if not mistral_api_key:
+        streamlit.info(_("Please add your Mistral API key to continue."), icon="🗝️")
     else:
 
-        # Create an Mistral client.
-        #client = Mistral(api_key=mistral_api_key)
+        # Create a Mistral client.
+        client_llm = Mistral(api_key=mistral_api_key)
 
         # Create a session state variable to store the chat messages. This ensures that the
         # messages persist across reruns.
         if "messages" not in streamlit.session_state:
             streamlit.session_state.messages = []
 
-        # Display the existing chat messages via `st.chat_message`.
+        # Display the existing chat messages via `streamlit.chat_message`.
         for message in streamlit.session_state.messages:
             with streamlit.chat_message(message["role"]):
                 streamlit.markdown(message["content"])
 
-        # Create a chat input field to allow the user to enter a message. This will display
+        # Create a chat text area to allow the user to enter a message. This will display
         # automatically at the bottom of the page.
-        if prompt := streamlit.chat_input("What is up?"):
+        user_description = streamlit.text_area(_("Please briefly describe your situation..."), height=150)
+        submit_button = streamlit.button(_("Send"))
+        if submit_button or streamlit.session_state.user_description != streamlit.session_state.get('user_description', ''):
+            if user_description:                
+                streamlit.session_state['user_description'] = user_description
+                streamlit.session_state.previous_user_description = streamlit.session_state.user_description
+                
+                streamlit.session_state.messages.append({"role": "user", "content": user_description})
+                with streamlit.chat_message("user"):
+                    streamlit.markdown(user_description)
 
-            # Store and display the current prompt.
-            streamlit.session_state.messages.append({"role": "user", "content": prompt})
-            with streamlit.chat_message("user"):
-                streamlit.markdown(prompt)
+                # Generate a response using the OpenAI API.
+                system_message = SystemMessage(content="")
+                user_message = UserMessage(content=user_description)
+                response = client_llm.chat.complete(
+                    model="mistral-large-latest",
+                    messages=[system_message, user_message],
+                    max_tokens=None,
+                    temperature=0.,
+                    )
+                answer = response.choices[0].message.content
+                _ = """ stream = client.chat.completions.create(
+                    model="mistral-nemo-12b",
+                    messages=[
+                        {"role": m["role"], "content": m["content"]}
+                        for m in streamlit.session_state.messages
+                    ],
+                    stream=True,
+                ) """
+                stream = string_to_stream(answer)
 
-            # Generate a response using the OpenAI API.
-            _ = """ stream = client.chat.completions.create(
-                model="mistral-nemo-12b",
-                messages=[
-                    {"role": m["role"], "content": m["content"]}
-                    for m in streamlit.session_state.messages
-                ],
-                stream=True,
-            ) """
-            stream = string_to_stream(prompt)
-
-            # Stream the response to the chat using `st.write_stream`, then store it in 
-            # session state.
-            with streamlit.chat_message("assistant"):
-                response = streamlit.write_stream(stream)
-            streamlit.session_state.messages.append({"role": "assistant", "content": response})
+                # Stream the response to the chat using `streamlit.write_stream`, then store it in 
+                # session state.
+                with streamlit.chat_message("assistant"):
+                    response = streamlit.write_stream(stream)
+                streamlit.session_state.messages.append({"role": "assistant", "content": response})
